4fc6f53a1f694b7215130ae232fc256563e663b3
Tobse <Tobse4Git@gmail.com>
2020-02-03 20:44:15 +0100

    🖱️ Add tile swap by mouse drag

--- src/commonMain/kotlin/j4k/candycrush/TileMover.kt
+++ src/commonMain/kotlin/j4k/candycrush/TileMover.kt
@@ -2,6 +2,7 @@ package j4k.candycrush
 
 import com.soywiz.klock.TimeSpan
 import com.soywiz.klock.seconds
+import com.soywiz.klogger.Logger
 import com.soywiz.korge.component.UpdateComponent
 import com.soywiz.korge.tween.get
 import com.soywiz.korge.tween.tweenAsync
@@ -13,32 +14,20 @@ import j4k.candycrush.MoveTileObserver.MoveTileEvent
 import j4k.candycrush.math.PositionGrid
 import j4k.candycrush.model.GameField
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+
+val log = Logger("TileMover")
 
 class TileMover(override val view: Stage,
         val renderer: GameFieldRenderer,
         val gameField: GameField,
         val positionGrid: PositionGrid) : MoveTileObserver.MoveTileListener, UpdateComponent {
 
-    private var moveTileEvent: MoveTileEvent? = null
 
     override fun update(ms: Double) {}
 
-    suspend fun updateAsync() {
-        val event = moveTileEvent
-        if (event != null) {
-            val start: Image? = renderer.getTile(event.start)
-            val startPos: IPoint = positionGrid.getPosition(event.start)
-            val end: Image? = renderer.getTile(event.end)
-            val endPos: IPoint = positionGrid.getPosition(event.end)
-            if (start != null && end != null) {
-                start.moveAsync(endPos)
-                end.moveAsync(startPos).invokeOnCompletion {
-                    renderer.swapTiles(event.start, event.end)
-                }
-            }
-            moveTileEvent = null
-        }
-    }
+    var movingTiles = false
+
 
     private suspend fun Image.moveAsync(point: IPoint, time: TimeSpan = 1.seconds): Deferred<Unit> {
         val easing = Easing.EASE_IN_OUT_ELASTIC
@@ -46,8 +35,40 @@ class TileMover(override val view: Stage,
     }
 
     override fun onMoveTileEvent(moveTileEvent: MoveTileEvent) {
-        this.moveTileEvent = moveTileEvent
-        println(moveTileEvent)
+        if (movingTiles) {
+            log.debug { "Omitted because of moving tiles: $moveTileEvent" }
+        } else {
+            swapTiles(moveTileEvent.start, moveTileEvent.end)
+        }
     }
+
+    fun swapTiles(start: PositionGrid.Position, end: PositionGrid.Position) {
+        val startPos: IPoint = positionGrid.getPosition(start)
+        val endPos: IPoint = positionGrid.getPosition(end)
+
+        val tileA: Image? = renderer.getTile(start)
+        val tileB: Image? = renderer.getTile(end)
+        if (tileA != null && tileB != null) {
+            log.debug { "$startPos - $endPos: $start-$end" }
+            swapTiles(tileA, endPos, tileB, startPos, start, end)
+        }
+    }
+
+    private fun swapTiles(tileA: Image,
+            endPos: IPoint,
+            tileB: Image,
+            startPos: IPoint,
+            start: PositionGrid.Position,
+            end: PositionGrid.Position) {
+        movingTiles = true
+        view.async {
+            tileA.moveAsync(endPos)
+            tileB.moveAsync(startPos).invokeOnCompletion {
+                renderer.swapTiles(start, end)
+                movingTiles = false
+            }
+        }
+    }
+
 
 }
--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,9 +1,8 @@
-import com.soywiz.klock.milliseconds
 import com.soywiz.klock.seconds
+import com.soywiz.klogger.Logger
 import com.soywiz.korge.Korge
 import com.soywiz.korge.tween.get
 import com.soywiz.korge.tween.tween
-import com.soywiz.korge.tween.tweenAsync
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.format.readBitmap
@@ -15,12 +14,17 @@ import j4k.candycrush.MoveTileObserver
 import j4k.candycrush.TileMover
 import j4k.candycrush.model.GameField
 
-suspend fun main() = Korge(width = 1280, height = 1024, bgcolor = Colors["#2b2b2b"]) {
+val log = Logger("main")
+
+suspend fun main() = Korge(width = 1280, height = 1024, bgcolor = Colors["#2b2b2b"], debug = true) {
     val minDegrees = (-16).degrees
     val maxDegrees = (+16).degrees
 
     val korgeBitmap = resourcesVfs["korge.png"].readBitmap()
     val gameField = GameField(8, 6)
+
+    Logger.defaultLevel = Logger.Level.DEBUG
+
 
     val testBitmap = resourcesVfs["test_stone.png"].readBitmap()
     val gameFieldRenderer = GameFieldRenderer(gameField, width.toInt(), height.toInt(), testBitmap)
@@ -34,17 +38,12 @@ suspend fun main() = Korge(width = 1280, height = 1024, bgcolor = Colors["#2b2b2
         scale(.2)
         position(1180, 80)
     }
+
     image.rotation(maxDegrees)
 
-
-    image.tweenAsync(image::globalX[20], time = 6.seconds, easing = Easing.EASE_IN_OUT)
-    image.tweenAsync(image::globalY[20], time = 6.seconds, easing = Easing.EASE_IN_OUT)
-
-
     while (true) {
-        image.tween(image::rotation[minDegrees], time = 50.milliseconds, easing = Easing.EASE_IN_OUT)
-        image.tween(image::rotation[maxDegrees], time = 50.milliseconds, easing = Easing.EASE_IN_OUT)
-        tileMover.updateAsync()
+        image.tween(image::rotation[minDegrees], time = 2.seconds, easing = Easing.EASE_IN_OUT)
+        image.tween(image::rotation[maxDegrees], time = 2.seconds, easing = Easing.EASE_IN_OUT)
     }
 
 }
