e25d29110ddd589c359e51ee1ac7f21113571cd4
Tobse <Tobse4Git@gmail.com>
2020-02-18 14:54:16 +0100

    🔛 Add projection to mouse event

--- src/commonMain/kotlin/j4k/candycrush/MoveTileObserver.kt
+++ src/commonMain/kotlin/j4k/candycrush/MoveTileObserver.kt
@@ -7,6 +7,7 @@ import com.soywiz.korge.component.MouseComponent
 import com.soywiz.korge.component.TouchComponent
 import com.soywiz.korge.view.View
 import com.soywiz.korge.view.Views
+import com.soywiz.korma.geom.Point
 import com.soywiz.korma.geom.distanceTo
 import j4k.candycrush.DragListener.DragEvent
 import j4k.candycrush.math.PositionGrid
@@ -32,8 +33,15 @@ class MoveTileObserver(override val view: View, private val grid: PositionGrid, 
     override fun onMouseEvent(views: Views, event: MouseEvent) {
         dragListener.onMouseEvent(views, event)
         if (event.button == MouseButton.RIGHT && event.type == MouseEvent.Type.DOWN) {
-            println("field: ${grid.getField(event.x, event.y)}(${event.x},${event.y})")
+            val point = event.point()
+            println("field: ${grid.getField(point.x, point.y)}(${point.x},${point.y})")
         }
+    }
+
+    fun MouseEvent.point() = project(Point(this.x, y))
+
+    fun project(point: Point): Point {
+        return view.globalToLocalXY(point.x, point.y, point)
     }
 
     override fun onDragEvent(dragEvent: DragEvent) {
